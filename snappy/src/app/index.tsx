464333be57578949af4fafc0d5a16e0782b9969e
Joseph Pendon <pendon.josephjr@gmail.com>
2026-05-19 22:21:27 +0800

    layout changes

--- snappy/src/app/index.tsx
+++ snappy/src/app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router";
 
 export default function Index() {
-  const isAuth = false; // replace with real auth check later
+  const isAuth = true; // replace with real auth check later
 
   return <Redirect href={isAuth ? "/(tabs)" : "/(auth)/login"} />;
 }
